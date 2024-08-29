21f28a45abf3b02ec33a2b04d702c60491928125
burakgoksu <gogsub58365@gmail.com>
2024-08-29 22:46:14 +0300

    alert bot

--- AlertAvailableSessions.py
+++ AlertAvailableSessions.py
@@ -49,8 +49,9 @@ class AlertAvailableSessions:
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=self.chrome_option)
         try:
             driver.get(self.link1)
             tc_no = driver.find_element(By.ID, 'txtTCPasaport')
